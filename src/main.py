f391926e58d5baf59d8ad7d9438dc990c8551591
Lucas Birkert <lucasbirkert@gmail.com>
2024-08-02 11:30:26 +0200

    Move gameloop into main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,28 @@
-from game import Game
+import pygame
+from sim import Simulation
 
-game = Game()
-game.start()
+simulation = Simulation()
+simulation.start()
+
+running = True
+pygame.init()
+screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
+clock = pygame.time.Clock()
+width, height = screen.get_size()
+    
+while running:
+    # process events sent by pygame
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        if event.type == pygame.VIDEORESIZE:
+            width, height = screen.get_size()
+
+    screen.fill("black")
+    
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+simulation.stop()
